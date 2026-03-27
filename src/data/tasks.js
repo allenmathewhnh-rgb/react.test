4d3068031717b1b084a086b686f251a34f4edfa2
Alen Mathew <allen.mathewhnh@gmail.com>
2026-03-27 10:49:20 +0530

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,6 @@
+export const initialTasks = [
+  { id: 1, text: "Learn React", completed: false },
+  { id: 2, text: "Practice Coding", completed: false },
+  { id: 3, text: "Build Project", completed: false },
+  { id: 4, text: "Test", completed: false }
+];
